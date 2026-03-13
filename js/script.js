e21981e2aad4d0e5ea442085c1f19e5f1509b21c
CRYPTRIXLEGEND <tanishbaliyan07@gmail.com>
2026-03-13 21:49:59 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,46 +1,29 @@
-
-// LOADING SCREEN (2 seconds)
-
+// LOADING SCREEN
 window.addEventListener("load", function(){
-
-setTimeout(function(){
-
-document.getElementById("loader").style.display="none";
-
-},2000);
-
+  setTimeout(function(){ document.getElementById("loader").style.display="none"; },2000);
 });
 
-
-
-// ANIMATED COUNTER
-
+// ANIMATED COUNTERS
 const counters=document.querySelectorAll(".counter");
-
 counters.forEach(counter=>{
+  counter.innerText='0';
+  const update=()=>{
+    const target=+counter.getAttribute("data-target");
+    const c=+counter.innerText;
+    const increment=target/100;
+    if(c<target){
+      counter.innerText=`${Math.ceil(c+increment)}`;
+      setTimeout(update,20);
+    }else{
+      counter.innerText=target;
+    }
+  };
+  update();
+});
 
-counter.innerText='0';
-
-const update=()=>{
-
-const target=+counter.getAttribute("data-target");
-const c=+counter.innerText;
-
-const increment=target/100;
-
-if(c<target){
-
-counter.innerText=`${Math.ceil(c+increment)}`;
-setTimeout(update,20);
-
-}
-
-else{
-counter.innerText=target;
-}
-
-};
-
-update();
-
+// HAMBURGER MENU
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+hamburger.addEventListener("click", ()=>{
+  navLinks.style.right = navLinks.style.right === "0px" ? "-100%" : "0px";
 });
