08769a8693ca3f7f6127ec593c4888c573f4de66
CRYPTRIXLEGEND <tanishbaliyan07@gmail.com>
2026-03-14 12:28:26 +0530

    Refactor counter animation and hamburger menu logic

--- js/script.js
+++ js/script.js
@@ -1,29 +1,61 @@
 // LOADING SCREEN
+
 window.addEventListener("load", function(){
-  setTimeout(function(){ document.getElementById("loader").style.display="none"; },2000);
+
+setTimeout(function(){
+
+document.getElementById("loader").style.display="none";
+
+},1500);
+
 });
 
-// ANIMATED COUNTERS
+
+// COUNTER ANIMATION
+
 const counters=document.querySelectorAll(".counter");
+
 counters.forEach(counter=>{
-  counter.innerText='0';
-  const update=()=>{
-    const target=+counter.getAttribute("data-target");
-    const c=+counter.innerText;
-    const increment=target/100;
-    if(c<target){
-      counter.innerText=`${Math.ceil(c+increment)}`;
-      setTimeout(update,20);
-    }else{
-      counter.innerText=target;
-    }
-  };
-  update();
+
+counter.innerText='0';
+
+const update=()=>{
+
+const target=+counter.getAttribute("data-target");
+
+const c=+counter.innerText;
+
+const increment=target/200;
+
+if(c<target){
+
+counter.innerText=`${Math.ceil(c+increment)}`;
+
+setTimeout(update,20);
+
+}
+
+else{
+
+counter.innerText=target;
+
+}
+
+};
+
+update();
+
 });
 
-// HAMBURGER MENU
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
-hamburger.addEventListener("click", ()=>{
-  navLinks.style.right = navLinks.style.right === "0px" ? "-100%" : "0px";
+
+// MOBILE MENU
+
+const hamburger=document.querySelector(".hamburger");
+
+const navLinks=document.querySelector(".nav-links");
+
+hamburger.addEventListener("click",()=>{
+
+navLinks.classList.toggle("active");
+
 });
