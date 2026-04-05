58979ebaf50f31cd2cf46bc622f86fa215a498cc
CRYPTRIXLEGEND <tanishbaliyan07@gmail.com>
2026-04-05 10:53:28 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,43 @@
 // LOADING SCREEN
 window.addEventListener("load", function(){
-  setTimeout(function(){
-    document.getElementById("loader").style.display="none";
-  },1500);
+  const loader = document.getElementById("loader");
+  if(loader){
+    setTimeout(function(){
+      loader.style.display="none";
+    },1500);
+  }
 });
 
 // COUNTER ANIMATION
 const counters = document.querySelectorAll(".counter");
-counters.forEach(counter => {
-  counter.innerText = '0';
-  const update = () => {
-    const target = +counter.getAttribute("data-target");
-    const c = +counter.innerText;
-    const increment = target / 200;
-    if(c < target){
-      counter.innerText = `${Math.ceil(c + increment)}`;
-      setTimeout(update,20);
-    } else {
-      counter.innerText = target;
-    }
-  };
-  update();
-});
+
+if(counters.length > 0){
+  counters.forEach(counter => {
+    counter.innerText = '0';
+
+    const update = () => {
+      const target = +counter.getAttribute("data-target");
+      const c = +counter.innerText;
+      const increment = target / 200;
+
+      if(c < target){
+        counter.innerText = `${Math.ceil(c + increment)}`;
+        setTimeout(update,20);
+      } else {
+        counter.innerText = target;
+      }
+    };
+
+    update();
+  });
+}
 
 // MOBILE MENU
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
+
+if(hamburger && navLinks){
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+}
